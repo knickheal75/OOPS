8ccac017c4f7fbc12dea7eabf6f1aabbc2b033c3
knickheal75 <blitzkrieg0705@gmail.com>
2019-02-03 21:52:21 +0530

    changes to oops demo

--- oop1.js
+++ oop1.js
@@ -1,1 +1,13 @@
-hhhh
+var Person=function(firstname,lastname,age)
+{
+this.firstname=firstname;
+this.lastname=lastname;
+this.age=age;
+};
+
+var person1=new Person('mark','reid',35);
+var person2=new Person('shane','austin',38);
+
+console.log('person details:'+ person1.firstname + person1.lastname + ' and age is'+ person1.age);
+console.log('person details:'+ person2.firstname + person2.lastname + ' and age is'+ person2.age);
+
